a4ae4954c9a20d24b2f4a7d2945ee48715b1a1f4
Todd Trivisonno <todd.trivisonno@gmail.com>
2020-01-09 16:06:54 -0500

    add smooth scroll

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import './App.css';
 import Navbar from './Components/Navbar.js';
 import Jumbotron from './Components/Jumbotron.js';
 import CardDeck from './Components/CardDeck.js';
